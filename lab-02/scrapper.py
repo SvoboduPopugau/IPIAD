ebdb92165314965d967cff0ca6e75b4a49b4f03c
SvoboduPopugau <Vlad42410@gmail.com>
2023-11-25 17:50:56 +0300

    ЛР 2

--- lab-02/scrapper.py
+++ lab-02/scrapper.py
@@ -1,4 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-class Scrapper:
+
+# Создаем экземпляр драйвера (например, ChromeDriver)
+driver = webdriver.Chrome()
+
+# Открываем веб-страницу
+driver.get("https://mail.ru/")
+
+# Находим элемент по его CSS-селектору
+element = driver.find_element(By.CSS_SELECTOR, "[title='Нефть марки Brent']")
+
+# Получаем текст из найденного элемента
+element_text = element.text
+
+# Выводим текст элемента
+print("Нефть марки Brent:", element_text)
+
+# Закрываем браузер
+driver.quit()
